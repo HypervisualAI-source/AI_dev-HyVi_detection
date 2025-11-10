34e33a369d8d025322a8bc5118e4c2e29ebfd9a3
HypervisualAI-source <chen@hypervisualai.com>
2025-11-11 08:56:21 +0900

    Add files via upload

---
+++ src/YOLOv13-N/inference.py
@@ -0,0 +1,19 @@
+import os                                                                      
+import sys                                                                                                                                                          
+                                                                                                                                                                                                                                                                                                                                                         
+sys.path.append(os.path.abspath('../../third_party_libraries'))                                                                        
+                                                                                                                                                                                             
+from ultralytics import YOLO                                                                             
+                                                                                                                                                                                                                              
+base_path = os.path.dirname(__file__)                                                                                             
+model_weight = os.path.join(base_path, 'models', 'yolov13n.pt')                                       
+
+model = YOLO(model_weight)                                                                                  
+
+results = model("test.jpg")                                                                  
+results[0].show()                                                                        
+
+
+                                                                                                                                                         
+                                                                              
+                                                                                                        
