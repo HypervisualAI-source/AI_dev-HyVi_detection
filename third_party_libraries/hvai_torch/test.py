4dce258f732fec124f01ac5c4d50d36172cc2ac0
HypervisualAI-source <chen@hypervisualai.com>
2026-01-15 17:39:48 +0900

    Update test.py

--- third_party_libraries/hvai_torch/test.py
+++ third_party_libraries/hvai_torch/test.py
@@ -12,7 +12,7 @@ Args:
 """
    
 # input data 
-input_test = torch.randn(1, 1000, 1024)   # (1, 50, 1024): [batch_size, token_size, embed_dim]
+input_test = torch.randn(1, 1000, 1024)   # (1, 1000, 1024): [batch_size, token_size, embed_dim]
         
 # case 1: multi-head attention
 mha_attention = nn.MultiheadAttention(embed_dim = 1024, num_heads = 16)  
@@ -28,3 +28,4 @@ hvai_mla = nn.MultiheadAttention(d_latent = 20, embed_dim = 1024, num_heads = 16
 with torch.no_grad():
     out_test = hvai_mla(input_test)
     print("out_test:", out_test.shape)
+
